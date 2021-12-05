e06dc5772e6210969507e321cef05a5f938a1484
ryn90 <ryn_90@hotmail.com>
2021-12-05 01:21:45 +0100

    Update weather.js

--- javascript/weather/weather.js
+++ javascript/weather/weather.js
@@ -38,7 +38,7 @@ form.addEventListener("submit", e => {
 
         if (filteredArray.length > 0) {
             msg.textContent = `You already know the weather for ${filteredArray[0].querySelector(".city-name span").textContent
-                } ...otherwise be more specific by providing the country code as well 😉`;
+                } ...otherwise be more specific by providing the country code too.`;
             form.reset();
             input.focus();
             return;
@@ -73,7 +73,7 @@ form.addEventListener("submit", e => {
             list.appendChild(li);
         })
         .catch(() => {
-            msg.textContent = "Please search for a valid city 😩";
+            msg.textContent = "Please search for a valid city.";
         });
 
     msg.textContent = "";
